748fb4a7ccd793fadf35916cb4dbcdd9b9bce398
taikob <taisukekbys@gmail.com>
2022-10-03 15:44:01 +0900

    add ignore vec if length from origin to point > radius

--- image_analysis/optical_flow.py
+++ image_analysis/optical_flow.py
@@ -4,29 +4,38 @@ import numpy as np
 colormap = {'blue': [255, 0, 0], 'green': [0, 255, 0], 'red': [0, 0, 255],
             'yellow': [0, 255, 255], 'white': [255, 255, 255]}
 
-def actsum(data):
+def act(data):
     sum=0.0
     for d in data:
         if d > 0: sum += d
-    return sum
 
-def supsum(data):
+    num = np.count_nonzero(data > 0)
+    if num != 0: ave = sum/num
+    else: ave = 0
+
+    return sum, ave, num
+
+def sup(data):
     sum = 0.0
     for d in data:
         if d < 0: sum += d
-    return sum
 
-def unit_vec(unitvec,vec,per=0):
-    norm = unitvec[0] * vec[0] + unitvec[1] * vec[1]
+    num = np.count_nonzero(data < 0)
+    if num != 0: ave = sum/num
+    else: ave = 0
+
+    return sum, ave, num
+
+def unit_vec(unitvec,vec,irt=0):
+    val = unitvec[0] * vec[0] + unitvec[1] * vec[1]
     vec=np.array(vec)
     vecnorm = np.sqrt(np.sum(np.abs(vec**2))) * np.sqrt(np.sum(np.abs(unitvec**2)))
 
-    if np.abs(norm)/vecnorm<per:
-        norm=0
+    if np.abs(val)/vecnorm < irt: val=0
 
-    return norm
+    return val
 
-def detect_rotmo(o,op,vec):#dimension of o and op must be 2
+def detect_rotmo(o,op,vec,rd=None):#dimension of o and op must be 2,o: origin, op: position from origin, rd: radius of circle
     #fix Inverted y axis for cv2 to correct axis
     o   = [  o[0], -   o[1]]
     op  = [ op[0], -  op[1]]
@@ -44,14 +53,15 @@ def detect_rotmo(o,op,vec):#dimension of o and op must be 2
     #The direction of rotation is CW (not CCW)
     r   =np.sqrt(r)
     norm=np.sqrt(norm)
+
+    if rd is not None and r>rd: return 0
+
     if r !=0 and norm < r:
         rotnorm=(vec[0]*opo[1] - vec[1]*opo[0]) /r
         if abs(rotnorm)/norm>=np.sqrt(2)/2:
             return rotnorm
-        else:
-            return 0
-    else:
-        return 0
+        else: return 0
+    else: return 0
 
 def save_flow_image(root, img, data, vs, met, cc = 'yellow', lc = 'red', s = 1, l = 2):
     mask = np.zeros_like(img)
@@ -74,16 +84,10 @@ def save_flow_image(root, img, data, vs, met, cc = 'yellow', lc = 'red', s = 1, 
     cv2.imwrite(os.path.join(root, 'result_' + met + '.jpg'), img)
 
 def save_flow_data(data, ofabs, root, met):
-    if np.count_nonzero(ofabs > 0) != 0:
-        aveact = actsum(ofabs) / np.count_nonzero(ofabs > 0)
-    else:
-        aveact = 0
-    if np.count_nonzero(ofabs < 0) != 0:
-        avesup = supsum(ofabs) / np.count_nonzero(ofabs < 0)
-    else:
-        avesup = 0
-    stdata = np.array([[actsum(ofabs), aveact, np.count_nonzero(ofabs > 0),
-                       supsum(ofabs), avesup, np.count_nonzero(ofabs < 0),
+    actsum, actave, actnum = act(ofabs)
+    supsum, supave, supnum = sup(ofabs)
+
+    stdata = np.array([[actsum, actave, actnum, supsum, supave, supnum,
                        ofabs.sum(), ofabs.mean(), len(ofabs)]])
 
     np.savetxt(os.path.join(root, 'statdata_'+ met+'.csv'), stdata, delimiter=',')
@@ -91,19 +95,16 @@ def save_flow_data(data, ofabs, root, met):
         writer = csv.writer(f, lineterminator='\n')
         writer.writerows(data)
 
-def lucas_kanade(img1, img2, dtct_rm=None, unitvec=None,per=None):
+def lucas_kanade(img1, img2, cnt=None, nvec=None, irt=None, rd=None):
     window_size= 50
     quality_level= 0.3
 
     # params for ShiTomasi corner detection
-    feature_params = dict(maxCorners = 100,
-                          qualityLevel = quality_level,
-                          minDistance = 7,
-                          blockSize = 7)
+    feature_params = dict(maxCorners = 100, qualityLevel = quality_level,
+                          minDistance = 7, blockSize = 7)
 
     # Parameters for lucas kanade optical flow
-    lk_params = dict(winSize = (window_size,
-                                window_size),
+    lk_params = dict(winSize = (window_size,window_size),
                      maxLevel = 2,
                      criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
     p0 = cv2.goodFeaturesToTrack(img1, mask = None, **feature_params)
@@ -119,31 +120,26 @@ def lucas_kanade(img1, img2, dtct_rm=None, unitvec=None,per=None):
 
             a, b = new.ravel()
             c, d = old.ravel()
-            dx = a - c
-            dy = b - d
-            if dtct_rm:
-                norm=detect_rotmo(dtct_rm,[c,d],[dx,dy])
-            elif unitvec:
-                norm=unit_vec(unitvec, [dx,dy], per=per)
-            else:
-                norm=np.sqrt(dx**2+dy**2)
+            dx = a - c; dy = b - d
+            if    cnt: val = detect_rotmo(cnt,[c,d],[dx,dy],rd=rd)
+            elif nvec: val = unit_vec(nvec, [dx,dy], irt=irt)
+            else:      val = np.sqrt(dx**2+dy**2)
 
-            data.append([c, d, dx, dy,norm])
-            if norm==0: continue
-            else: ofabs = np.append(ofabs, norm)
+            data.append([c, d, dx, dy,val])
+            if val==0: continue
+            else: ofabs = np.append(ofabs, val)
 
         return data,ofabs
     except:
         return None,None
 
-def farneback(img1, img2, dtct_rm=None, unitvec=None, per=None):
+def farneback(img1, img2, cnt=None, nvec=None, irt=None, rd=None):
     window_size= 10
     stride= 5
     min_vec= 0.01
     try:
         flow = cv2.calcOpticalFlowFarneback(img1, img2, None, 0.5, 3,
-                                            window_size,
-                                            3, 5, 1.2, 0)
+                                            window_size, 3, 5, 1.2, 0)
         height, width = img1.shape
 
         data = []
@@ -152,29 +148,27 @@ def farneback(img1, img2, dtct_rm=None, unitvec=None, per=None):
                                       range(0, height, stride)):
             if np.linalg.norm(flow[y, x]) >= min_vec:
                 dx, dy = flow[y, x].astype(float)
-                if dtct_rm:
-                    norm=detect_rotmo(dtct_rm,[x,y],[dx,dy])
-                elif unitvec:
-                    norm=unit_vec(unitvec, [dx,dy], per=per)
-                else:
-                    norm=np.sqrt(dx**2+dy**2)
-                data.append([x, y, dx, dy, norm])
-                ofabs = np.append(ofabs, norm)
+                if    cnt: val = detect_rotmo(cnt,[x,y],[dx,dy],rd=rd)
+                elif nvec: val = unit_vec(nvec, [dx,dy], irt=irt)
+                else:      val = np.sqrt(dx**2+dy**2)
+                data.append([x, y, dx, dy, val])
+                ofabs = np.append(ofabs, val)
         return data,ofabs
     except:
         return None,None
 
-def get_optical_flow(root,file1, file2, met='lk',cc='yellow',lc='red',vs=None,s=1,l=2, dtct_rm=False, unitvec=False, per=False):
+def get_optical_flow(root,file1, file2, met='lk',cc='yellow',lc='red',vs=None,s=1,l=2, cnt=False, nvec=False, irt=False, rd=False):
+    #cnt: center of circle, nvec: unit vector, irt: ignore rate, rd: radius
     img1 = cv2.imread(os.path.join(root,file1))
     img2 = cv2.imread(os.path.join(root,file2))
     img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
     if met=='lk':
-        data,ofabs=lucas_kanade(img1_gray, img2_gray, dtct_rm, unitvec,per)
+        data, ofabs = lucas_kanade(img1_gray, img2_gray, cnt, nvec,irt,rd)
         if vs is None: vs=50
     elif met=='fb':
-        data,ofabs=farneback(img1_gray, img2_gray, dtct_rm, unitvec,per)
+        data, ofabs = farneback(img1_gray, img2_gray, cnt, nvec,irt,rd)
         if vs is None: vs=4
     if data is None or ofabs is None:
         stdata=np.array([[0,0,0,0,0,0,0,0,0]])
