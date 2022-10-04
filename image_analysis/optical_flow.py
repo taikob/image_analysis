e960d6cc04a00052c9e01ec5a9bbc24349d05a2d
taikob <taisukekbys@gmail.com>
2022-10-04 10:42:54 +0900

    fix detect_rotmo that calc angular velocity

--- image_analysis/optical_flow.py
+++ image_analysis/optical_flow.py
@@ -57,9 +57,9 @@ def detect_rotmo(o,op,vec,rd=None):#dimension of o and op must be 2,o: origin, o
     if rd is not None and r>rd: return 0
 
     if r !=0 and norm < r:
-        rotnorm=(vec[0]*opo[1] - vec[1]*opo[0]) /r
-        if abs(rotnorm)/norm>=np.sqrt(2)/2:
-            return rotnorm
+        rotval=(vec[0]*opo[1] - vec[1]*opo[0]) /(r*r)
+        if abs(rotval)/norm>=np.sqrt(2)/2:
+            return rotval
         else: return 0
     else: return 0
 
